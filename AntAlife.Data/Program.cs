bf5d15d142b60758192cc1c8a14e9f95d858420a
Дмитрий <129512956+Qargo1@users.noreply.github.com>
2025-04-18 20:21:41 +0330

    Initial?2

---
+++ AntAlife.Data/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AntAlife.Data
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AntAlife.Logic/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AntAlife.Logic
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
